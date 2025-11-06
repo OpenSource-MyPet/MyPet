2d78738af632f54b387bafe905e798d48c347d04
seunghyee <qkqdl12345@naver.com>
2025-11-07 03:17:08 +0900

    Add Kakao Map UI

---
+++ map-ui/src/main/java/com/mysite/sbb/MapUiApplication.java
@@ -0,0 +1,11 @@
+package com.mysite.sbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapUiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MapUiApplication.class, args);
+    }
+}
